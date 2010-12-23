0de07bb088ace1078f277dac042eaa3ce1a13993
Hugo.cor <Hugo.cor@29eba4b7-8064-8cd4-fd9a-dc11107ae07a>
2010-12-23 01:48:59 +0000

    Here is my eclipse project. MoteJ needs bluecove and slf4j.

---
+++ FitAR.Drone/src/ARDrone.java
@@ -0,0 +1,203 @@
+
+/**
+ * @author Hugo Cordier
+ */
+
+
+import java.net.*;
+
+public class ARDrone implements Runnable {
+   
+	private final int ALTTITUDE_MAX = 1;
+	private final byte[] IP_ADDR={new Integer(192).byteValue(),new Integer(168).byteValue(),new Integer(1).byteValue(),new Integer(1).byteValue()};
+	private DatagramSocket socket;
+	private InetAddress inet_addr;
+	
+	private int seq = 1;
+	private float pitch = 0, roll = 0, gaz = 0, yaw = 0;
+    private float step = (float)0.7;
+    
+	private boolean flying=false;
+	private boolean rollPitchAllowed = true;
+	private boolean takeoffAllowed = false;	
+
+	/**
+	 * Setting up ARDrone.
+	 * ARDronne class will communicate with the drone
+	 */
+    public ARDrone() throws Exception {
+		inet_addr = InetAddress.getByAddress(IP_ADDR);
+		socket = new DatagramSocket();
+		socket.setSoTimeout(3000);	
+		setMaxAltitude(ALTTITUDE_MAX);
+    }
+    
+	/**
+	 * Commands are sent every 20ms to the drone (see updateDrone()).
+	 */
+    public void run(){
+    	while(!Thread.interrupted()){
+    		try {
+				Thread.sleep(20l);
+				if(seq > Integer.MAX_VALUE-10)
+					seq = 0;
+				updateDrone();
+			} catch (InterruptedException e) {
+			} catch (Exception e) {
+			}
+    	}
+    }
+    
+    /*Setters*/
+	/**
+	 * Set boolean to allow the drone to take off
+	 */
+    public void setTakeoffAllowed(boolean takeoffAllowed) {
+		this.takeoffAllowed = takeoffAllowed;
+	}
+	/**
+	 * Set boolean to allow roll and pitch movements.
+	 * This can be use to set up a security button in order to avoid random movements with accelerometer or analog device
+	 */
+    public void setRollPitchAllowed(boolean rollPitchAllowed){
+    	this.rollPitchAllowed=rollPitchAllowed;
+    }
+
+	/**
+	 * Make the drone going up
+	 */
+	public void gazUp() {this.gaz=step;}
+	
+	/**
+	 * Make the drone going down
+	 */
+	public void gazDown() {this.gaz=-step;}
+	
+	/**
+	 * Make the drone turning left
+	 */
+	public void left() {	this.yaw=-step;}
+	
+	/**
+	 * Make the drone turning right
+	 */
+	public void right() {this.yaw=step;}
+	
+	/**
+	 * Make the drone going ahead or back.
+	 * @param pitch A float between -1 and 1
+	 */
+	void setPitch(float pitch){
+		if(rollPitchAllowed && pitch>-1 && pitch<1)
+			this.pitch = pitch;
+	}
+	/**
+	 * Make the drone going right or left
+	 * @param roll A float between -1 and 1
+	 */
+	public void setRoll(float roll){
+		if(rollPitchAllowed && roll>-1 && roll<1)
+			this.roll = roll;
+	}
+	/**
+	 * Stop up/down and turn left/right movements
+	 */
+	public void stopMovment(){
+		this.gaz=0;
+		this.yaw=0;
+	}
+    
+	/**
+	 * Ask the drone to send video from video port (5555)
+	 */
+	public void initVideo(){
+		try {
+			send_to_video("AT*ZAP="+ (seq++) + ",0");
+		} catch (Exception e) {
+			System.out.println("Can't ask for video stream");
+		}
+	}
+	
+	/**
+	 * Make the drone taking off    
+	 */
+    public void takeoff(){
+    	try {
+    		if(!flying && takeoffAllowed){
+    			send_at_cmd("AT*REF=" + (seq++) + ",290718208");
+    			flying=true;
+    			System.out.println("Taking off");
+    		}
+		} catch (Exception e) {
+			System.err.println("Can't take off !");
+		}
+    	
+    }
+    
+    /**
+     * Make the drone landing
+     */
+    public void land(){
+    	try {
+    		if(flying){
+    			send_at_cmd("AT*REF=" + (seq++) + ",290717696");
+    			flying=false;
+    			System.out.println("Landing");
+    		}
+		} catch (Exception e) {
+			System.err.println("Can't land !");
+		}
+    }
+    
+    /**
+     * Setting drone's max altitude for the flight
+     * @param maxAlt The maximum altitude
+     */
+    public void setMaxAltitude(int maxAlt) {
+		try {
+			send_at_cmd("AT*CONFIG=1,\"control:altitude_max\",\""+(maxAlt*1000)+"\"\"");
+			System.out.println("Setting max altitude = "+maxAlt);
+		} catch (Exception e) {
+			System.err.println("Unable to set maximum altitude !");
+		}
+    }
+
+    /**
+     * Send updated information to the drone
+     */
+    private void updateDrone() {
+    	try {
+    		send_at_cmd( "AT*PCMD=" + (seq++) + ",1," 
+    			+ Float.floatToIntBits(pitch) + "," 
+    			+ Float.floatToIntBits(roll) + "," 
+    			+ Float.floatToIntBits(gaz) + "," 
+    			+ Float.floatToIntBits(yaw));
+			send_at_cmd("AT*COMWDG="+(seq++) );
+		} catch (Exception e) {
+			System.err.println("Unable to send information to drone ! Check connection");
+		}
+    }
+    
+    /*UDP connection manager*/
+    /**
+     * Send the AT command to the drone
+     * @param at_cmd the command to send to the drone
+     * @throws An exception if packet can't be sent (unable to find a network for exemple)
+     */
+    private void send_at_cmd(String at_cmd) throws Exception {
+		byte[] buffer = (at_cmd + "\r").getBytes();
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, inet_addr, 5556);
+		socket.send(packet);
+    }
+    
+    /**
+     * Send a command to vidéo port 
+     * @param at_cmd
+     * @throws Exception
+     */
+    private void send_to_video(String at_cmd) throws Exception {
+		byte[] buffer = (at_cmd + "\r").getBytes();
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, inet_addr, 5555);
+		socket.send(packet);
+    }
+}
---
+++ FitAR.Drone/src/WiimoteManager.java
@@ -0,0 +1,117 @@
+
+/**
+ * @author Hugo Cordier
+ */
+
+
+import motej.Mote;
+import motej.MoteFinder;
+import motej.MoteFinderListener;
+import motej.event.ExtensionEvent;
+import motej.event.ExtensionListener;
+import motej.request.ReportModeRequest;
+import motejx.extensions.balanceboard.BalanceBoard;
+import motejx.extensions.nunchuk.*;
+
+public class WiimoteManager implements MoteFinderListener,ExtensionListener{
+
+	private Mote wiimote;
+	private Nunchuk nunchuk;
+	private BalanceBoard balanceboard;
+	private MoteFinder finder;
+	private Object lock = new Object();
+	private ARDrone drone;
+	
+	public static void main(String[] args) throws Exception {
+		new WiimoteManager();
+	}
+	
+	/**
+	 * Set up connection with the wiimote and the nunchuk.
+	 */
+	private WiimoteManager(){
+		/*Searching for a wiimote*/
+		System.out.println("Searching for a wiimote..." +
+				"\nPress 1 and 2 simultaneously." +
+				"\nThis wil take about 10s");
+		this.finder = MoteFinder.getMoteFinder();
+		this.finder.addMoteFinderListener(this);
+		this.finder.startDiscovery();
+		try {
+			synchronized(lock) {
+				lock.wait();
+			}
+		} catch (InterruptedException ex) {
+			System.err.println("Can't find wiimote");
+		}
+		/*Setting up wiimote config*/
+		try {
+			this.drone = new ARDrone();
+			System.out.println("Let's fly !!");
+			this.wiimote.addCoreButtonListener(new WiimoteListener(this.drone));
+			Thread threadDrone = new Thread(drone);
+			threadDrone.start();
+			System.out.println("Please connect a nunchuk to your wiimote")	;
+		} catch (Exception e) {
+			System.err.println(e);
+			System.err.println("Can't initiate drone");
+		}
+	}
+	
+	/**
+	 * Function called when a mote has been found.
+	 * @param mote The wiimote which have just be found
+	 */
+	@Override
+	public void moteFound(Mote mote) {
+		this.finder.stopDiscovery();
+		this.wiimote = mote;
+		this.wiimote.setPlayerLeds(new boolean[] {true, false, false, true} );
+		mote.addExtensionListener(this);
+		synchronized(lock) {
+			lock.notifyAll();
+		}
+	}
+
+	/**
+	 * Method called when an extension is connected to the wiimote.
+	 * If this is a nunchuk, user is informed and ardrone is allowed to take off.
+	 * @param evt The connection event
+	 */
+	@Override
+	public void extensionConnected(ExtensionEvent evt) {
+		if (evt.getExtension() instanceof Nunchuk) {
+			System.out.println("Nunchuk found");
+			this.wiimote.setPlayerLeds(new boolean[] {true, true, false, true} );
+			this.nunchuk = (Nunchuk) evt.getExtension();
+			this.nunchuk.addAnalogStickListener(new NunchukListener(this.drone));
+			this.wiimote.setReportMode(ReportModeRequest.DATA_REPORT_0x32);
+			drone.setTakeoffAllowed(true);
+		}else
+		if (evt.getExtension() instanceof BalanceBoard) {
+			System.out.println("BalanceBoard found");
+			this.wiimote.setPlayerLeds(new boolean[] {true, true, true, true} );
+			this.balanceboard = (BalanceBoard) evt.getExtension();
+			this.balanceboard.addBalanceBoardListener(new boardListener(this.drone));
+			this.wiimote.setReportMode(ReportModeRequest.DATA_REPORT_0x32);
+			drone.setTakeoffAllowed(true);
+		}
+	}
+
+	/**
+	 * Method called when an extension is disconnected.
+	 * Ardrone is stopped and will not be allowed to take off until a new nunchuk is connected
+	 * @param evt The disconnection event
+	 */
+	@Override
+	public void extensionDisconnected(ExtensionEvent evt) {
+		if (evt.getExtension() instanceof BalanceBoard) {
+			System.out.println("BalanceBoard disconnected");
+		}else if (evt.getExtension() instanceof Nunchuk) {
+			System.out.println("Nunchuk disconnected");
+		}
+		this.wiimote.setPlayerLeds(new boolean[] {true, false, false, true} );
+		drone.land();
+		drone.setTakeoffAllowed(false);
+	}
+}
